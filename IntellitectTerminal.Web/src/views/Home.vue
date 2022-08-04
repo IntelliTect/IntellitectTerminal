999cce3f27a33c79e0786ea9acd352260dbf0528
Ahmad Moltafet <55408986+amoltafet@users.noreply.github.com>
2022-08-04 16:43:11 -0700

    Added welcome message with help (#22)
    * added welcome message with help
    
    * fixed logic
    
    * added fullscreen
    
    * added fullscreen

--- IntellitectTerminal.Web/src/views/Home.vue
+++ IntellitectTerminal.Web/src/views/Home.vue
@@ -6,26 +6,56 @@
   import { Terminal } from "xterm";
 
   @Component
-  export default class Home extends Vue {
+  export default class CoalesceExample extends Vue {
+    
+    helpView(term : Terminal) {
+              term.write("\r\n");
+              term.write("help - Displays this message");
+              term.write("\r\n");
+              term.write("ls - Lists all files in the current directory");
+              term.write("\r\n");
+              term.write("cat - Displays the contents of a file");
+              term.write("\r\n");
+              term.write("mkdir - Creates a directory");
+              term.write("\r\n");
+              term.write("challenge - Requests a challenge");
+              term.write("\r\n");
+              term.write("submit - Submits a challenge");
+              term.write("\r\n");
+              term.write("edit - Edits a file");
+              term.write("\r\n");
+              term.write("progress - Displays your progress");
+              term.write("\r\n");
+              term.write("verify - Verifies a challenge");
+              term.write("\r\n");
+              term.write('\x1B[1;3;31mIntelliTect\x1B[0m $ ');
+    }
 
     async created() {
-      var temp: string = "";
-      var term = new Terminal();
-      const input = document.getElementById('terminal');
-      if (input != null) {
-        term.open(document.getElementById('terminal') as HTMLElement);
-        term.write('Hello from \x1B[1;3;31mIntelliTect\x1B[0m $ ');
+      var userInput: string = "";      
+      var term = new Terminal({ cursorBlink: true });
+      var welcomeMessage = "Welcome to the Intellitect CLI! View commands by typing help";
+      if (userInput != null) {
+        term.open(document.querySelector("#terminal") as HTMLElement);
+        term.write(welcomeMessage);
+        term.write("\r\n");
+        term.write('\x1B[1;3;31mIntelliTect\x1B[0m $ ');
         term.onKey(e => {
-          console.log(e.key);
-          temp += e.key;
+          userInput += e.key;
           term.write(e.key);
           if (e.key == '\r') {
-            term.write('\n');
-            term.write('Hello from \x1B[1;3;31mIntellitect\x1B[0m $ ');
-            temp = "";
-            console.log('cleared temp');
+            if (userInput == "help\r") {
+              this.helpView(term);
+            } else {
+              term.write("\r\n");
+              term.write('Command not found. Type "help" for a list of commands.');
+              term.write("\r\n");
+              term.write('\x1B[1;3;31mIntelliTect\x1B[0m $ ');
+            }
+            userInput = "";
           }
-        })
+        })  
+        
       }
     }
   }
