fb002a9ce6aa4f4196aa01566ac1ff98e52a5a14
Ahmad Moltafet <amoltafet@zagmail.gonzaga.edu>
2022-08-04 15:52:09 -0700

    fixed logic

--- IntellitectTerminal.Web/src/views/Home.vue
+++ IntellitectTerminal.Web/src/views/Home.vue
@@ -4,28 +4,13 @@
 <script lang="ts">
   import { Component, Prop, Vue } from "vue-property-decorator";
  import { Terminal } from "xterm";
+import { FitAddon } from 'xterm-addon-fit';
 
 
   @Component
   export default class CoalesceExample extends Vue {
 
-    async created() {
-      var userInput: string = "";
-      var term = new Terminal({cursorBlink: true,});
-      const input = document.getElementById('terminal');
-      var welcomeMessage = "Welcome to the Intellitect CLI! View commands by typing help";
-
-      if (input != null) {
-        term.open(document.getElementById('terminal') as HTMLElement);
-        term.write(welcomeMessage);
-        term.write("\r\n");
-        term.write('\x1B[1;3;31mIntelliTect\x1B[0m $ ');
-        term.onKey(e => {
-          //console.log(e.key);
-          userInput += e.key;
-          term.write(e.key);
-          if (e.key == '\r') {
-            if (userInput == "help\r") {
+    helpView(term : Terminal) {
               term.write("\r\n");
               term.write("help - Displays this message");
               term.write("\r\n");
@@ -46,8 +31,29 @@
               term.write("verify - Verifies a challenge");
               term.write("\r\n");
               term.write('\x1B[1;3;31mIntelliTect\x1B[0m $ ');
-            } 
-            else {
+    }
+
+    async created() {
+      var userInput: string = "";
+      const fitAddon = new FitAddon();
+      var term = new Terminal({ cursorBlink: true });
+      term.loadAddon(fitAddon);
+      fitAddon.fit();
+      const input = document.getElementById('terminal');
+      var welcomeMessage = "Welcome to the Intellitect CLI! View commands by typing help";
+      if (input != null) {
+        term.open(document.getElementById('terminal') as HTMLElement);
+        term.write(welcomeMessage);
+        term.write("\r\n");
+        term.write('\x1B[1;3;31mIntelliTect\x1B[0m $ ');
+        term.onKey(e => {
+          //console.log(e.key);
+          userInput += e.key;
+          term.write(e.key);
+          if (e.key == '\r') {
+            if (userInput == "help\r") {
+              this.helpView(term);
+            } else {
               term.write("\r\n");
               term.write('Command not found. Type "help" for a list of commands.');
               term.write("\r\n");
