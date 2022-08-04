0f9a15bbc50e421c364822d8b3e30b3c448a5146
Benjamin Michaelis <benjamin@michaelis.net>
2022-08-04 14:16:51 -0700

    Add in xterm

--- IntellitectTerminal.Web/src/views/Home.vue
+++ IntellitectTerminal.Web/src/views/Home.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="home">
-    <!-- Component is auto-imported by `unplugin-vue-components`
-    (either kebab-case or PascalCase works.) -->
-    <HelloWorld msg="Welcome to your new Vue.js Coalesce application!" />
-  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+  import { Component, Prop, Vue } from "vue-property-decorator";
+  import { Terminal } from "xterm";
 
-@Component({})
-export default class Home extends Vue {}
+  @Component
+  export default class CoalesceExample extends Vue {
+
+    async created() {
+      var temp: string = "";
+      var term = new Terminal();
+      const input = document.getElementById('terminal');
+      if (input != null) {
+        term.open(document.getElementById('terminal') as HTMLElement);
+        term.write('Hello from \x1B[1;3;31mIntelliTect\x1B[0m $ ');
+        term.onKey(e => {
+          console.log(e.key);
+          temp += e.key;
+          term.write(e.key);
+          if (e.key == '\r') {
+            term.write('\n');
+            term.write('Hello from \x1B[1;3;31mIntellitect\x1B[0m $ ');
+            temp = "";
+            console.log('cleared temp');
+          }
+        })
+      }
+    }
+  }
 </script>
