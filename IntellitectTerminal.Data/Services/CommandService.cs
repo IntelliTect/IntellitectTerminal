812c501a35afc0c38e34404b902274b4a25751e1
Benjamin Michaelis <benjamin@michaelis.net>
2022-08-04 19:39:11 -0700

    Work on Request Command

--- IntellitectTerminal.Data/Services/CommandService.cs
+++ IntellitectTerminal.Data/Services/CommandService.cs
@@ -15,12 +15,28 @@ public class CommandService : ICommandService
 
     public Challenge RequestCommand(Guid? userId)
     {
+        // Check if userId is a Guid
+        if (userId is null)
+        {
+        }
+
         Challenge challenge = new();
-        challenge.ChallengeId = 1;
+
+        User foundUser = Db.Users.Where(x => x.UserId == userId).FirstOrDefault() ?? CreateAndSaveNewUser();
+
+        // Interpret file system to determine what challenge they are on
         challenge.Level = 1;
         challenge.Question = "This is the question";
         challenge.Answer = "You did it right!";
         challenge.CompilationLanguage = Challenge.CompilationLanguages.None;
         return challenge;
+    }
+
+    private User CreateAndSaveNewUser()
+    {
+        User user = new User { UserId = Guid.NewGuid() };
+        Db.Users.Add(user);
+        Db.SaveChanges();
+        return user;
     }
 }
