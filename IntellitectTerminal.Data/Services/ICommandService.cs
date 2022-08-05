2ef2366fb11784938b08164c7829886a0cd63a74
Benjamin Michaelis <benjamin@michaelis.net>
2022-08-05 00:58:46 -0700

    Set correct access to endpoints

--- IntellitectTerminal.Data/Services/ICommandService.cs
+++ IntellitectTerminal.Data/Services/ICommandService.cs
@@ -1,4 +1,5 @@
 ﻿using IntelliTect.Coalesce;
+using IntelliTect.Coalesce.DataAnnotations;
 using IntellitectTerminal.Data.Models;
 
 namespace IntellitectTerminal.Data.Services;
@@ -6,5 +7,6 @@ namespace IntellitectTerminal.Data.Services;
 [Coalesce, Service]
 public interface ICommandService
 {
+    [Execute(PermissionLevel = SecurityPermissionLevels.AllowAll)]
     Challenge Request(Guid? userId);
 }
